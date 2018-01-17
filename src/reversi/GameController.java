62c908b06a053dd7e640c34acd73ced5479e194e
Shlomy Trajber <shlomytra@gmail.com>
2018-01-17 21:11:34 +0200

    Finished the game process

--- src/reversi/GameController.java
+++ src/reversi/GameController.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import logic.*;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -22,8 +23,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
-    private static String black,white;
-    private static HumanPlayer BPlayer,WPlayer;
+    private static String black, white;
+    private static HumanPlayer BPlayer, WPlayer;
     private static DeafultLogic logic;
     private static Board board;
     private static boolean firstsTurn;
@@ -41,7 +42,8 @@ public class GameController implements Initializable {
     @FXML
     private Label player2L;
 
-    @FXML private void exitToMenu() {
+    @FXML
+    private void exitToMenu() {
         try {
             Stage primaryStage = (Stage) this.exit.getScene().getWindow();
             VBox root = (VBox) FXMLLoader.load(getClass().getResource("ReversiMenu.fxml"));
@@ -63,29 +65,29 @@ public class GameController implements Initializable {
         try {
             reader = new BufferedReader(new InputStreamReader(new FileInputStream(FILENAME)));
             boardSize = (Integer.parseInt(reader.readLine()));
-            black= reader.readLine();
-            white= reader.readLine();
+            black = reader.readLine();
+            white = reader.readLine();
 
         } catch (IOException e) {
             boardSize = 8;
-            black= "Black";
-            white= "White";
-            sendAlert("Error: Error reading file", "The default settings will be used.");
+            black = "Black";
+            white = "White";
+            sendAlert(0,"Error: Error reading file", "The default settings will be used.");
         }
-        firstsTurn=true;
-        currentPlayer.setText("Current Player: "+ black);
-        player1L.setText(black+ " Score: 2");
-        player2L.setText(white+ " Score: 2");
+        firstsTurn = true;
+        currentPlayer.setText("Current Player: " + black);
+        player1L.setText(black + " Score: 2");
+        player2L.setText(white + " Score: 2");
         board = new Board(boardSize);
         logic = new DeafultLogic(board);
         BPlayer = new HumanPlayer(logic);
         WPlayer = new HumanPlayer(logic);
-        logic.possibleMoves(firstsTurn,2,null);
-        gridBoard = new GridBoard(board,logic,BPlayer,WPlayer, black,white);
-        gridBoard.setOnMouseClicked(event ->{
+        logic.possibleMoves(firstsTurn, 2, null);
+        gridBoard = new GridBoard(board, logic, BPlayer, WPlayer, black, white);
+        gridBoard.setOnMouseClicked(event -> {
             double x = event.getX();
             double y = event.getY();
-            Point p = getClickedPoint(x,y);
+            Point p = getClickedPoint(x, y);
             tryToPlay(p);
         });
         gridBoard.setPrefWidth(PANELSIZE);
@@ -94,40 +96,69 @@ public class GameController implements Initializable {
         root.getChildren().add(0, gridBoard);
     }
 
-    private static Point getClickedPoint(double x, double y){
+    private static Point getClickedPoint(double x, double y) {
         Double sizeOfCell = ((double) (PANELSIZE)) / ((double) (board.getBoardSize()));
         double x1 = x / sizeOfCell;
         double y1 = y / sizeOfCell;
-        return new Point((int)x1,(int)y1);
+        return new Point((int) x1, (int) y1);
     }
 
 
-    private void tryToPlay(Point p){
-        if(logic.possibleMoves(firstsTurn,2,null) ){
-            if (board.getPosCell(p.getX(), p.getY())){
-                logic.move(firstsTurn, p.getX(),p.getY());
-                gridBoard.draw();
-                firstsTurn=!firstsTurn;
-                player1L.setText(black+ " Score: "+board.getBTiles());
-                player2L.setText(white+ " Score: "+board.getWTiles());
-                if (firstsTurn)
-                    currentPlayer.setText("Current Player: "+ black);
-                else
-                    currentPlayer.setText("Current Player: "+ white);
-            }
+    private void tryToPlay(Point p) {
+        logic.possibleMoves(firstsTurn, 2, null);
+        boolean isNextMoveExist = true;
+        if (board.getPosCell(p.getX(), p.getY())) {
+            logic.move(firstsTurn, p.getX(), p.getY());
+            firstsTurn = !firstsTurn;
+            isNextMoveExist = logic.possibleMoves(firstsTurn, 2, null);
+            gridBoard.draw();
+            player1L.setText(black + " Score: " + board.getBTiles());
+            player2L.setText(white + " Score: " + board.getWTiles());
+            if (firstsTurn)
+                currentPlayer.setText("Current Player: " + black);
             else
-                sendAlert("Invalid Move", "please chose a valid move");
-        }
-        else {
-            sendAlert("No Move", "There is no possible move\n The turn goes to the other player");
-            firstsTurn=!firstsTurn;
+                currentPlayer.setText("Current Player: " + white);
+        } else if (!board.isFull())
+            sendAlert(1 ,"Invalid Move", "please chose a valid move");
+        if (board.isFull())
+            gameOver();
+        if (!isNextMoveExist && !board.isFull()) {
+            sendAlert(1,"No Move", "There is no possible move\nThe turn goes to the other player");
+            firstsTurn = !firstsTurn;
+            isNextMoveExist = logic.possibleMoves(firstsTurn, 2, null);
+            gridBoard.draw();
+            player1L.setText(black + " Score: " + board.getBTiles());
+            player2L.setText(white + " Score: " + board.getWTiles());
+            if (firstsTurn)
+                currentPlayer.setText("Current Player: " + black);
+            else
+                currentPlayer.setText("Current Player: " + white);
+            if (!isNextMoveExist)
+                gameOver();
         }
 
     }
 
+    private void gameOver(){
+        String winner;
+        if (board.getBTiles() > board.getWTiles())
+            winner="Black!";
+        else if (board.getBTiles() < board.getWTiles())
+            winner="White!";
+        else
+            winner="no one! The game ended as a draw!";
 
-    public void sendAlert(String header, String contact) {
+        sendAlert(1,"The winner is "+ winner,"The game is finished!\nThe final score is: Black = " + board.getBTiles() + "\tWhite = "
+                + board.getWTiles());
+
+    }
+
+
+
+    public void sendAlert(int type, String header, String contact) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
+        if(type == 1)
+            alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText(header);
         alert.setContentText(contact);
         alert.showAndWait();
