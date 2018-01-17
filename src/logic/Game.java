67e62c90b1449b585269a047a0599c6ba41a50fc
Shlomy Trajber <shlomytra@gmail.com>
2018-01-17 12:16:57 +0200

    Opend a package to the logic

---
+++ src/logic/Game.java
@@ -0,0 +1,78 @@
+package logic;
+
+public class Game {
+
+    private Player black;
+    private Player white;
+    private Board board;
+    private GameLogic logic;
+    private Printer printer;
+    private  boolean blacksTurn;
+
+    /**
+     * the function creates new instance of logic.Game
+     * @param board instances of the board, the players, and the game's logic
+     * @param
+     * @param
+     * @param
+     * @param
+     **/
+
+    Game(Board board, Player black, Player white, GameLogic logic, Printer printer) {
+        this.board = board;
+        this.white = white;
+        this.black = black;
+        this.logic = logic;
+        this.printer = printer;
+        blacksTurn = true;
+    }
+
+    /**
+     * @name : playGame
+     * @parameters : no parameters
+     * @return : the function runs the game
+     **/
+
+    public void playGame() {
+        // In the first turn there should not be a message of last position
+        boolean notFirstTurn = false;
+
+        while (isGameNotOver()) {
+            printer.printBoard(board, false);
+            if (blacksTurn) {
+                if (notFirstTurn)
+                    printer.printLastMove(blacksTurn, white.getLastMove().getX(), white.getLastMove().getY());
+                notFirstTurn = true;
+                printer.printNextTurn(blacksTurn);
+                if (logic.possibleMoves(blacksTurn,black.getType(),printer))
+                    black.playOneTurn(blacksTurn);
+                else {
+                    black.declareNoMoves();
+                    printer.printNoMoves();
+                }
+            } else {
+                printer.printLastMove(blacksTurn, black.getLastMove().getX(), black.getLastMove().getY());
+                printer.printNextTurn(blacksTurn);
+                if (logic.possibleMoves(blacksTurn,white.getType(), printer))
+                    white.playOneTurn(blacksTurn);
+                else {
+                    white.declareNoMoves();
+                    printer.printNoMoves();
+                }
+            }
+            blacksTurn = !blacksTurn;
+        }
+
+        printer.printsWhoWon(board);
+    }
+
+    /**
+     * @name : isGameNotOver
+     * @parameters : no parameters
+     * @return : the function returns true if the game is over or not
+     **/
+
+    private boolean isGameNotOver() {
+        return (!board.isFull() && (black.getCanPlay() || white.getCanPlay()));
+    }
+}
---
+++ src/logic/HumanPlayer.java
@@ -0,0 +1,37 @@
+package logic;
+
+import java.util.Scanner;
+
+public class HumanPlayer extends Player {
+
+    protected Printer printer;
+    private Scanner scanner = new Scanner(System.in);
+
+    /**
+     * the constructor
+     * @param  gameLogic gameLogic
+     * @param  printer printer
+     **/
+    HumanPlayer(GameLogic gameLogic, Printer printer) {
+        super(gameLogic);
+        this.canPlay = true;
+        this.type = 1;
+        this.printer = printer;
+    }
+
+
+    @Override
+    void playOneTurn(boolean blacksTurn) {
+        this.canPlay = true;
+        printer.printEnterNextTurn();
+        int row, col;
+        row = scanner.nextInt();
+        col = scanner.nextInt();
+        lastMove.setX(row - 1);
+        lastMove.setY( col - 1);
+        logic.move(blacksTurn, row -1, col - 1);
+    }
+
+
+
+}
